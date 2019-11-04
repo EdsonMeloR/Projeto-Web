99049e1bb674af5a7f09de7ca426364c7ff39ca4
lucas.hcarvalho4 <lucas.hcarvalho4@ITQ0626039W10-1>
2019-11-04 17:04:47 -0300

    Alterações de ordem do menu

--- index.php
+++ index.php
@@ -15,17 +15,20 @@
         <nav class="menu" id="menu">
             <ul>
                 <li><a href="#primeira">Inicio</a></li>
-                <li><a href="#segunda">Serviços</a></li>
-                <li><a href="#setima">Planos</a></li>
-                <li><a href="#sexta">Localização</a></li>
-                <li><a href="#quarta">Sobre</a></li>
-                <li><a href="#quinta">Contato</a></li>
+                <li><a href="#segunda">Quem somos</a></li>
+                <li><a href="#terceira">Serviços</a></li>
+                <li><a href="#quarta">Planos</a></li>
+                <li><a href="#quinta">Localização</a></li>
+                <li><a href="#sexta">Contato</a></li>
             </ul>
         </nav>
     </header>
 
     <main>
-        <!--Conteúdo 1-->
+
+    <!-- ==================================================================================================== -->
+
+        <!--Conteúdo 1 - Inicio-->
         <section class="primeira parallax eltitulo" id="primeira">
             <h1>P R O S P E R I T Y   L O G</h1>
             <br>
@@ -35,18 +38,38 @@
             <p>Excelência e confiabilidade em transportes</p>
         </section>
 
-        <!--Conteúdo 2-->
-        <section class="segunda parallax eltitulo" id="segunda">    
-        <h1>S E R V I Ç O S</h1>
+    <!-- ==================================================================================================== -->
+
+        <!--Conteúdo 2 - Quem somos-->
+    <section class="segunda parallax eltitulo" id="segunda">    
+        <h1>Quem somos</h1>
         <br>
-        <!--  Blocos -->
+        <section class="conteudo1 eltexto">
+            <p>Excelência e confiabilidade em transportes</p>
+        </section>
+    </section>
+
+    <section class="conteudo2 eltexto">
+            <!-- <p>A Prosperity Log trabalha com diversos tipos de transportes</p> -->
+            
+    </section>
+
+    <!-- ==================================================================================================== -->
+
+
+        <!--Conteúdo 3 - Planos-->
+    <section class="terceira parallax eltitulo" id="terceira">
+            <h1>S E R V I Ç O S</h1>
+            <br> 
+
+            <!--  BLOCOS -->
+
         <article class="quadrado fadeIn">
             <img src="img/01.png" alt=""width="350px" height="350px">
             <p class="subtxt">Operador Logístico</p>
         
         </article>
         
-
         <article class="quadrado fadeIn">
             <img src="img/02.png" alt=""width="350px" height="350px">
             <p class="subtxt">Entregas urgentes</p>
@@ -56,43 +79,36 @@
             <img src="img/03.png" alt=""width="350px" height="350px">
             <p class="subtxt">Transporte de Carga e Distribuição</p>
         </article>
+
     </section>
 
+    <!-- ==================================================================================================== -->
 
-        <section class="conteudo2 eltexto">
-            <!-- <p>A Prosperity Log trabalha com diversos tipos de transportes</p> -->
-            
-        </section>
-
-        <!--Conteúdo 3-->
-        <section class="terceira parallax eltitulo" id="terceira">
-            <h1>L O C A L I Z A Ç Ã O</h1>
+        <!--Conteúdo 4 - Localização-->
+    <section class="quarta parallax eltitulo" id="quarta">
+            <h1>P L A N O S</h1>
             <br>
-            
+             
+    </section>
 
-        <section class="conteudo3 eltexto">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.214046137869!2d-46.44973638502215!3d-23.
-                560754784683215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce667d14e063b1%3A0x4c9e04085f9ba8f6!2sR.%2
-                0Francisco%20Velho%2C%20175%20-%20Vila%20Carmosina%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2008290-060!5e0!3m2!1spt-BR
-                !2sbr!4v1572637843275!5m2!1spt-BR!2sbr" width="1903" height="470" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
-            </section>
-         </section>
-
-        <!--Conteúdo 4-->
-        <section class="quarta parallax eltitulo" id="quarta">
-            <h1>L O C A L I Z A Ç Ã O</h1>
-            <br>
-            <div class="maps">
-            
-            </div>
-        </section>
-
-        <section class="conteudo4 eltexto">
+    <section class="conteudo4 eltexto">
             <p>A startup ShellCode está localizada em Itaquera</p> 
-         </section>
+    </section>
 
-        <!--Conteúdo 5-->
+    <!-- ==================================================================================================== -->
+
+
+        <!--Conteúdo 5 - Contato-->
         <section class="quinta parallax eltitulo" id="quinta">
+            <h1>L O C A L I Z A Ç Ã O</h1>
+            <br>
+        </section>
+
+    <!-- ==================================================================================================== -->
+
+
+        <!--Conteúdo 5 - Sobre-->
+        <section class="quarta parallax eltitulo" id="sexta">
             <h1>C O N T A T O</h1>
             <br>
             <form class="formulario">
@@ -115,25 +131,26 @@
                     <div class="btn">
                     <button type="Enviar">Enviar sua mensagem</button>
                 </div>
+                
             </form>
         </section>
+
+        <section class="conteudo4 eltexto">
+            <p>A startup ShellCode está localizada em Itaquera</p> 
+         </section>
+
+    <!-- ==================================================================================================== -->
 
         <section class="conteudo5 eltexto">
             <p>Gostou do nosso trabalho? Entre em contato conosco!</p> 
          </section>
 
-         <!-- Footer -->
+         <!-- Footer - Rodapé -->
          <footer class="rodape container bg-gradient">
-            <div class="social-icons">
-                <a href="https://www.facebook.com/LucasSecond"><i class="fa fa-facebook"></i></a>
-                <a href="https://www.instagram.com/luscas.mp4/?hl=pt-br"><i class="fa fa-instagram"></i></a>
-                <a href="https://www.behance.net/LucasSecond"><i class="fa fa-behance"></i></a>
-                <a href="https://www.youtube.com/channel/UCURZdjU9IgWfexyBtjXz02w?view_as=subscriber"><i class="fa fa-youtube"></i></a>
-            </div>
+            
             <p class="copyright">
                 Copyright © ShellCode 2019. Todos os direitos reservados.
             </p>
-
             <p class="adm">Área Administrativa</p>
          
     </main>
