55be65ac1c814f35276df891cfc0021be65fedd6
lucas.hcarvalho4 <lucas.hcarvalho4@ITQ0626039W10-1>
2019-11-05 14:49:46 -0300

    Correções de bug

---
+++ admin/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Área Administrativa</title>
+    <!-- ! Links de CSS -->
+    <link rel="stylesheet" href="css/estilo.css">
+
+</head>
+<body>
+    <div id="box-login">
+        <div id="formulario-login">
+            <form id="frmlogin" name="frmlogin" action="" method="POST">
+                <fieldset>
+                    <legend>Faça seu login</legend>
+                    
+                    <label for=""><span>Login</span></label>
+                    <input type="text" name="login_user" id="login_user">
+
+                    <label for=""><span>Senha</span></label>
+                    <input type="password" name="senha_user" id="senha_user">
+
+                    <input type="submit" name="logar_user" id="logar" value="Logar" class="botao">
+                </fieldset>
+            </form>
+    
+    </div>
+</body>
+</html>
