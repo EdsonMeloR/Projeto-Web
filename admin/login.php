d2b51017767282a261078f0f6d79245136021efb
lucas.hcarvalho4 <lucas.hcarvalho4@ITQ0626039W10-1>
2019-11-06 14:26:31 -0300

    Correções - Tela de login

--- admin/login.php
+++ admin/login.php
@@ -5,7 +5,7 @@
     <title>Área Administrativa</title>
     <!-- ! Links de CSS -->
     <link rel="stylesheet" href="css/estilo.css">
-    <link rel="shortcut icon" href="img/icone.png">
+    <link rel="shortcut icon" href="../img/icone.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
 
@@ -20,7 +20,7 @@
 
             <div class="field_forms">
                 <label for="user_name">
-                    <i class="icon fa-fauser"></i>
+                    <i class="icon fa fa-user"></i>
                     <span class="hidden">Usuário</span>
                 </label>
                 <input class="filed_form" type="text" name="txt_adm" placeholder="Usuario">
@@ -30,7 +30,7 @@
 
             <div class="field_forms">
                 <label for="user_name">
-                    <i class="icon fa-fauser"></i>
+                    <i class="icon fa fa-lock"></i>
                     <span class="hidden">Usuário</span>
                 </label>
                 <input class="filed_form" type="password" name="senha_adm" placeholder="Senha">
@@ -41,8 +41,11 @@
             <div class="field_forms">
                 <input type="submit" value="Fazer Login">
             </div>
-
-        </form>
-    </div>    
+        </form>    
+    </div>  
+    
+    <p class="copyright">
+                Copyright © ShellCode 2019. Todos os direitos reservados.
+    </p>
 </body>
 </html>
