6721413a13c15cb2155a75dd14859c7eab8a0795
Kaue emanuel <kaueramos2017@gmail.com>
2019-11-06 13:01:53 -0300

    Ajuste da tela de login do Administrador(fazer os icones)

--- admin/login.php
+++ admin/login.php
@@ -6,23 +6,43 @@
     <!-- ! Links de CSS -->
     <link rel="stylesheet" href="css/estilo.css">
     <link rel="shortcut icon" href="img/icone.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
 
 </head>
 
-<body>
-    <div id="box-login">
-        <div id="formulario-login">
-            <form id="frmlogin" name="frmlogin" action="" method="POST">
-                <fieldset>                    
-                    <input type="text" name="login_user"placeholder="Usuário" id="login_user">
-                    <br>                    
-                    <input type="password" name="senha_user" placeholder="Senha" id="senha_user">
-                    <br>
-                    <input class="btn-form radius"type="submit" name="logar_user" id="logar" value="Logar" class="botao">
-                </fieldset>
-            </form>
-    
-    </div>
+<body class="alinhamento">
+
+    <div class="box">
+
+        <form action="op_adm.php" class="frm login">
+            <!-- USUARIO -->
+
+            <div class="field_forms">
+                <label for="user_name">
+                    <i class="icon fa-fauser"></i>
+                    <span class="hidden">Usuário</span>
+                </label>
+                <input class="filed_form" type="text" name="txt_adm" placeholder="Usuario">
+            </div>
+
+            <!-- SENHA -->
+
+            <div class="field_forms">
+                <label for="user_name">
+                    <i class="icon fa-fauser"></i>
+                    <span class="hidden">Usuário</span>
+                </label>
+                <input class="filed_form" type="password" name="senha_adm" placeholder="Senha">
+            </div>
+
+            <!-- BOTÃO -->
+
+            <div class="field_forms">
+                <input type="submit" value="Fazer Login">
+            </div>
+
+        </form>
+    </div>    
 </body>
 </html>
