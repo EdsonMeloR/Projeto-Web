e6134cf6c57ca6a1c80329c262b116632472b332
kaue.elramos <kaue.ramos2017@gmail.com>
2019-11-22 16:58:04 -0300

    ajuste geral

--- admin/frm_adicionarUser.php
+++ admin/frm_adicionarUser.php
@@ -1,19 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+
+    <style>
+        .valido 
+        {
+            border: 10px solid green;
+        }
+
+        .invalido 
+        {
+            border: 10px solid red;
+        }
+        
+    </style>
+    <!-- jQuery -->
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+        
+        <!-- Funções para validação de CPF e CNPJ -->
+        <script src="js/valida_cpf_cnpj.js"></script>
+        
+        <!-- EXEMPLOS -->
+        
+        <!-- Pressionando teclas -->
+        <script src="js/metodo1.js"></script>
+</head>
+<body>
+
+<input class="cpf_cnpj">
+
 <div class="form-principal">
     <form action="" class="form-contato" method="POST" tabindex="1">
 
         <span class="frm_titulo">Novo Usuário</span>
 
-        <input type="text" class="input-contato" name="" placeholder="Nome" required>
+        <input type="text" class="input-contato" name="ins_neme_user" placeholder="Nome" required>
 
-        <input type="text" class="input-info" name="" placeholder="CPF" required>
+        <input class="cpf_cnpj" name="ins_cpf_user" placeholder="CPF" required>
 
-        <input type="text" class="input-info" name="" placeholder="Telefone" required>
+        <input type="text" class="input-contato" name="ins_telefone_user" placeholder="Telefone" required>
 
-        <input type="text" class="input-contato" name="" placeholder="Email" required>
+        <input type="text" class="input-contato" name="ins_email_user" placeholder="Email" required>
 
-        <input type="text" class="input-contato" name="" placeholder="Senha" required>
+        <input type="text" class="input-contato" name="ins_senha_user" placeholder="Senha" required>
 
-        <input type="text" class="input-contato" name="" placeholder="Confirmar Senha" required>
+        <input type="text" class="input-contato" name="ins_ConfirmaSenha_user" placeholder="Confirmar Senha" required>
 
         <input type="submit" class="btn_form" value="Enviar">
     </form>
@@ -32,7 +65,7 @@
 	    <tbody>
 		    <tr>
 			    <td>1002</td>
-			    <td>Carlos</td>
+			    <td>Carlos Eduardo Souza</td>
 			    <td>22070854</td>
                 <td>cadu.edu@gmail.com</td>
                 <td>12345698708</td>
@@ -42,3 +75,8 @@
 	    </tbody>
     </table>
 </div>
+    
+</body>
+</html>
+
+
