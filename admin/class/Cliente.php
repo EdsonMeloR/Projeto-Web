016dbab8890b941c1041780658692f926724d960
carlos.espsilva <carlosespsilva@outlook.com>
2019-11-25 15:43:28 -0300

    Adição das classes e exclusão da classe user

--- admin/class/Cliente.php
+++ admin/class/Cliente.php
@@ -98,6 +98,160 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // public function insert()
+        // {
+        //     $sql = new Sql();
+        //     $resultado = $sql->select("CALL insert_usuario(:nome, :cpf, :tel, :senha, :email, :nvl)",
+        //     array
+        //     (
+        //         ':nome'=>$this->getNome(),
+        //         ':cpf'=>$this->validaCPF(getCpf()),
+        //         ':telefone'=>$this->getTelefone(),
+        //         ':senha'=>md5($this->getSenha()),
+        //         ':email'=>$this->getEmail(),
+        //         ':nvl'=>$this->getNivel()
+        //     ));
+        //     if(count($resultado)>0)
+        //     {
+        //         $this->setData($resultado[0]);
+        //     }
+        // }
+
+        // //* Alterar Usuario
+        // public function Alterar()
+        // {
+        //     $sql = new Sql();
+        //     $sql->select("CALL update_usuario(:telefone, :email, :id)", 
+        //     array
+        //     (
+        //         ':telefone'=>$this->getTelefone(),
+        //         ':email'=>$this->getEmail(),
+        //         ':id'=>$this->getId()
+        //     ));
+        // }
+
+        // //* Efetuar Login
+        // public function EfetuarLogin($_cpf, $_senha)
+        // {
+        //     $sql = new Sql();
+        //     $_senhac = md5($_senha); //todo: Senha c = senha Criptografada
+        //     $resultado = $sql->select('SELECT * FROM usuario WHERE Cpf = :Cpf AND Senha = :Senha',
+        //     array
+        //     (
+        //         ':Cpf' => $_cpf,
+        //         ':Senha' => $_senhac
+        //     ));
+        //     if(count($resultado)>0)
+        //     {
+        //         $this->setData($resultado[0]);
+        //     }
+        // }
+        
+        //* Listar Usuario
+
+
+
+
+
+        // //* Gerar Lista adm
+        // //* '::' chama um metodo estático da classe 
+        // public static function getList()
+        // {
+        //     $sql = new Sql();
+        //     return $sql->select('SELECT * FROM administrador');
+        //     //* Realiza um busca no banco trazendo tudo que é administrador onde o criterio é nome.
+        // }
+        // //* Pesquisa por parte do nome
+        // //todo: 'LIKE' junto ao '%'ed'%' retorna tudo que tenha 'ed' no nome inteiro como: 'Edson' e 'Eduardo' entre outros.
+        // public static function search($nome_adm)
+        // {
+        //     $sql = new Sql();
+        //     return $sql->select('SELECT * FROM administrador WHERE nome LIKE :nome', array(':nome'=>"%".$nome_adm."%"));
+        // }
+        // //* Concluir o login 
+        // public function efetuarlogin($_login, $_senha)
+        // {
+        //     $sql = new SQL();
+        //     $senhaMd5 = md5($_senha);
+        //     $results = $sql->select('SELECT * FROM administrador WHERE login = :login AND senha = :senha', 
+        //     array
+        //     (
+        //         ':login'=>$_login,
+        //         ':senha'=>$senhaMd5
+        //     ));
+        //     if(count($results)>0)
+        //     {
+        //         $this->setData($results[0]);
+        //     }
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
         
 
     }
